430c3b8ad173514f7237782a0a54a070ef5b858e
nigaees <147188831+nigaees@users.noreply.github.com>
2024-09-25 21:36:25 +0300

    Add files via upload

---
+++ Zxeni.lua
@@ -0,0 +1,19 @@
+-- Updated Script [ Added New Tab + Script Rizz ]
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("", "DarkTheme")
+
+
+local Test = Window:NewTab("Main")
+local TestSection = Test:NewSection("Main")
+
+TestSection:NewButton("Tariviste KJ Script", "Key : YieldingLeaks_ui34y9eYBchxlga", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tariviste/Gojo/main/Source"))()
+end)
+
+local Test = Window:NewTab("Fuckin Words")
+local TestSelection = Test:NewSection("Main")
+
+TestSelection:NewButton("Minecraft Script", "Not a faek itz rel 😛", function()
+    print("minecraft sex update 12.10.24")
+end)
